f19910992fa903c9bf93c9f4f0b7d7d577bd80f8
BiancaReset <130721122+BiancaReset@users.noreply.github.com>
2023-07-21 18:35:18 +0000

    21julio

---
+++ src/js/component/Home.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import '../../styles/index.css'
+import TodoApp from "./TodoApp";
+
+//create your first component
+const Home = () => {		
+		  return (
+			<div>
+			  <TodoApp />
+			</div>
+		  );
+		};
+export default Home;
+
+
+
+
+
---
+++ src/js/component/TodoApp.jsx
@@ -0,0 +1,49 @@
+import React, { useState } from 'react';
+
+const TodoApp = () => {
+  const [todos, setTodos] = useState([]);
+  const [inputValue, setInputValue] = useState('');
+
+  const handleInputChange = (e) => {
+    setInputValue(e.target.value);
+  };
+
+  const handleAddTodo = () => {
+    if (inputValue.trim() !== '') {
+      setTodos([...todos, inputValue]);
+      setInputValue('');
+    }
+  };
+
+  const handleDeleteTodo = (index) => {
+    const updatedTodos = todos.filter((_, i) => i !== index);
+    setTodos(updatedTodos);
+  };
+
+  return (
+    <div className="todo-container">
+      <h1>Todo App</h1>
+      <div className="input-container">
+        <input
+          type="text"
+          value={inputValue}
+          onChange={handleInputChange}
+          placeholder="Enter your todo..."
+        />
+        <button onClick={handleAddTodo}>Add</button>
+      </div>
+      <ul>
+        {todos.map((todo, index) => (
+          <li key={index}>
+            {todo}
+            <button onClick={() => handleDeleteTodo(index)}>Delete</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default TodoApp;
+
+
